f745262dc59da03ec97aefa8704fe0d5e5e92b6c
sanszch <sandzarisakov461@gmail.com>
2025-12-13 21:02:05 +0100

    day 1: python basics day 1 finished

--- 1_week/1st_day.py
+++ 1_week/1st_day.py
@@ -1,4 +1,4 @@
-# Day 1 — Python basics
+"""# Day 1 — Python basics
 
 # 1. Переменные
 age = 20
@@ -26,4 +26,32 @@ print(f"My name is {name}, I am {age} years old")
 user_name = input("Enter your name: ")
 user_age = int(input("Enter your age: "))
 
-print(f"Hello {user_name}, next year you will be {user_age + 1}")
+print(f"Hello {user_name}, next year you will be {user_age + 1}")"""
+"""Посчитать площадь круга (радиус = 7)
+
+Перевести минуты в часы и минуты
+
+Найти среднее из 3 чисел
+
+Вывести своё имя 5 раз
+
+Поменять местами a и b"""
+# radius = float(input("Enter a radius: "))
+# area = (radius ** 2) * 3.14159
+# print(f"The area is {area:.2f}")
+
+# seconds = 7322
+# hours = seconds // 3600
+# print(hours)
+# rem_seconds = seconds % 3600
+# print(rem_seconds)
+# minutes = rem_seconds // 60
+# rem_seconds = rem_seconds % 60
+# print()
+
+
+a = int(input("First digit:"))
+b = int(input("Second digit:"))
+c = int(input("Third digit:"))
+average = float((a+b+c)/ 3)
+print(f'Average is {average:.2f}')
