4ec86f11c1c385697d9724569a89d56b2a0a5045
sanszch <sandzarisakov461@gmail.com>
2025-12-17 13:23:34 +0100

    day 1: python basics day 1 finished day 2 finished day 3 finished day 4 finished day 5 finished

---
+++ 1_week/5th_day.py
@@ -0,0 +1,17 @@
+"""Task 1"""
+# text = "  Data Science is COOL  "
+# text = text.strip()
+# text = text.lower()
+# print(text)
+"""Task 3"""
+# sentence = "Python is very powerful language"
+# sentence = sentence.split()
+# print(len(sentence))
+"""Task 4"""
+# word = "analytics"
+# for i in word:
+#     print(i)
+# print(len(word))
+"""Task 5"""
+# text = "Data science is fun"
+# print(text.replace("fun", "awesome"))
