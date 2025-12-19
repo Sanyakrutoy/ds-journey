2ed02651efe652e5b26c5fec34b9684b66d1d764
sanszch <sandzarisakov461@gmail.com>
2025-12-19 11:21:16 +0100

    day 1: python basics day 1 finished day 2 finished day 3 finished day 4 finished day 5 finished day 6 finished

---
+++ 1_week/6th_day.py
@@ -0,0 +1,22 @@
+"""Task 1"""
+# def square(a):
+#     print(a ** 2)
+# square(6)
+"""Task 2"""
+# def even_or_odd(a):
+#     print("Even" if a % 2 == 0 else "Odd")
+# even_or_odd(int(input("enter a digit : ")))
+"""Task 3"""
+# def list_sum(a):
+#     total = 0
+#     for i in a:
+#         total += i
+#     print(total)
+# list_sum([2,3,4])
+"""Task 4"""
+# def len_of_str(a):
+#     a = a.split()
+#     return len(a)
+# result = len_of_str("Привет сан")
+# print(result)
+
